54970f799ea8a159a9653fe4fb2c94717641d6ef
Facundo Marrero <facu_marrero@hotmail.com>
2022-10-30 11:07:17 -0300

    se agrega servicio y Product (capa de domain)

--- src/main/java/com/platzi/market/web/controller/MundoController.java
+++ src/main/java/com/platzi/market/web/controller/MundoController.java
@@ -1,21 +1,20 @@
 package com.platzi.market.web.controller;
 
-import com.platzi.market.persistence.ProductosCrudRepository;
+import com.platzi.market.persistence.ProductoRepository;
+import com.platzi.market.persistence.entity.Producto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.websocket.server.PathParam;
-
 @RestController
 @RequestMapping("/saludar")
 public class MundoController {
-
+    @Autowired // esto se usa para que te cree la instancia Spring
+    private ProductoRepository productos;
     @GetMapping("/hola/{id}")
     public String saludar(@PathVariable int id){
-
-        ProductosCrudRepository productos = new ProductosCrudRepository();
         return productos.getProductoNombre(id);
     }
 }
